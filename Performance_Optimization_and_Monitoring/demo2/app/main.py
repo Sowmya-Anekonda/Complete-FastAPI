4f988474d6591601f27625ed110f5f5d123fc76e
Sowmya Anekonda <amsowmya.anekonda@gmail.com>
2026-02-22 15:30:09 +0530

    added FastAPI with Prometheus, Graphana and Docker

---
+++ Performance_Optimization_and_Monitoring/demo2/app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
+
+app = FastAPI()
+
+Instrumentator().instrument(app).expose(app)
+
+
+@app.get('/')
+def root():
+    return {'message': 'Fastapi with Promentheus, Graphana & Docker!'}
+
+@app.get('/ping')
+def ping():
+    return {'message': 'pong'}
